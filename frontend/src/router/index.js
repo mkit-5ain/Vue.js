965b25890fb1e83a7dbc60546d24f52302741ca1
mkit-5ain <mkit5ain@gmail.com>
2021-12-22 18:25:23 +0900

    [update] router add, script modify

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,25 +1,36 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../pages/Home.vue'
+import Experience from '../pages/Experience.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue')
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: () => import('../pages/Contact.vue')
+    },
+    {
+        path: '/experience',
+        name: 'Experience',
+        component: Experience
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
