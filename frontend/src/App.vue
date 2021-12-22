579e730dfdf93a81d1a10d35c599f2a3fe77fe1d
mkit-5ain <mkit5ain@gmail.com>
2021-12-22 18:34:54 +0900

    [update] component create

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,46 +3,18 @@
     <article id="container">
         <router-view/>
     </article>
-    <div class="cursor"></div>
+    <ui-cursor/>
 </template>
 <script>
-    import Header from '@/components/Header.vue';
-    export default {
-        name: 'App',
-        components: {
-            'ui-header': Header
-        },
-        mounted: function () {
-            this.cursorPointer(),
-            this.cursorActive();
-        },
-        methods: {
-            cursorPointer() {
-                const cursor = document.querySelector(".cursor");
-
-                document.addEventListener('mousemove', (e) => {
-                    const mouseX = e.clientX;
-                    const mouseY = e.clientY;
-                    cursor.style.left = mouseX + 'px';
-                    cursor.style.top = mouseY + 'px';
-                    cursor.style.opacity = '1';
-                });
-
-            },
-            cursorActive() {
-                const cursor = document.querySelector(".cursor");
-                const cursorLink = document.querySelectorAll(".scale");
-
-                cursorLink.forEach(ele => ele.addEventListener("mouseover", () => {
-                    cursor.style.transform = 'scale(3.2)';
-                }));
-
-                cursorLink.forEach(ele => ele.addEventListener("mouseout", () => {
-                    cursor.style.transform = 'scale(1)';
-                }));
-            }
-        }
+import Header from '@/components/Header.vue';
+import Cursor from '@/components/Cursor.vue';
+export default {
+    name: 'App',
+    components: {
+        'ui-header': Header,
+        'ui-cursor': Cursor
     }
+}
 </script>
 <style lang="scss">
     @import '@/assets/sass/common.scss';
---
+++ frontend/src/components/Cursor.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cursor"></div>
+</template>
+<script>
+export default {
+    name: 'Curosr',
+    mounted: function () {
+        this.cursorPointer(),
+        this.cursorActive();
+    },
+    methods: {
+        cursorPointer() {
+            const cursor = document.querySelector(".cursor");
+
+            document.addEventListener('mousemove', (e) => {
+                const mouseX = e.clientX;
+                const mouseY = e.clientY;
+                cursor.style.left = mouseX + 'px';
+                cursor.style.top = mouseY + 'px';
+                cursor.style.opacity = '1';
+            });
+
+        },
+        cursorActive() {
+            const cursor = document.querySelector(".cursor");
+            const cursorLink = document.querySelectorAll(".scale");
+
+            cursorLink.forEach(ele => ele.addEventListener("mouseover", () => {
+                cursor.style.transform = 'scale(3.2)';
+            }));
+
+            cursorLink.forEach(ele => ele.addEventListener("mouseout", () => {
+                cursor.style.transform = 'scale(1)';
+            }));
+        }
+    }
+}
+</script>
