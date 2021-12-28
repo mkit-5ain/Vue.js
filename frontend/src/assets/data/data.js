03c50d860bbd18cbab96bb3adc41841d4cf7e359
mkit-5ain <mkit5ain@gmail.com>
2021-12-28 18:52:49 +0900

    [create] data

---
+++ frontend/src/assets/data/data.js
@@ -0,0 +1,62 @@
+[
+    {
+        "title": "AAAAAAAA",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "BBBBBBBB",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "CCCCCCCC",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "DDDDDDDD",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "EEEEEEEE",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    },
+    {
+        "title": "FFFFFFFF",
+        "href": "index.html",
+        "url":"../image/common/hover-banner-01.jpg"
+    }
+]
