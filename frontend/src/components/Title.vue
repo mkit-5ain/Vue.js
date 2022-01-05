e51adcd9ada87bb8a3569d484e5f86d745a23314
mkit-5ain <mkit5ain@gmail.com>
2022-01-05 17:43:32 +0900

    [update] scroll css modify

--- frontend/src/components/Title.vue
+++ frontend/src/components/Title.vue
@@ -25,19 +25,22 @@
     	text-align: center;
     	letter-spacing: -7px;
     	text-transform: uppercase;
-    	div {
-    		position: relative;
-    		color: #cdc6be;
-    		transform: rotate(1.2deg);
+        &.active {
+            div { color: #cdc6be; transform: rotate(1.2deg);
+                &:after { width: 100%; }
+            }
+        }
+    	div { position: relative; color: #fff; transition: 1s ease; transform: rotate(0deg);
     		&:after {
     			content: '';
     			position: absolute;
     			top: 0px;
     			left: 0px;
-    			width: 100%;
+    			width: 0%;
     			height: 100%;
     			background: $black-color;
     			z-index: -1;
+                transition: 1s ease;
     		}
     	}
     }
